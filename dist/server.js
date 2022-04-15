39c49089d310b561d78dee2259cc393100d3389e
Dev-js <fdr.desenvolvedor@gmail.com>
2022-04-15 15:09:01 -0300

    Add files via upload

--- dist/server.js
+++ dist/server.js
@@ -5,6 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 app.listen(3000, () => {
     console.log(`server running port 3000 for project: ${process.env.PROJECT_NAME}`);
 });
